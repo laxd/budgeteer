b8575f7c75a208aba0be6fd34dd81b5c679b8a2e
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2020-02-03 20:33:16 +0000

    Added intitial model classes

--- app.js
+++ app.js
@@ -4,6 +4,34 @@ var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('sqlite::memory:');
+
+const Budget = sequelize.define('Budget', {
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true
+    },
+    name: DataTypes.STRING
+},{
+    sequelize,
+    timestamps: true
+})
+
+const Category = sequelize.define('Category', {
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true
+    },
+    name: DataTypes.STRING
+}, {
+    sequelize
+})
+
+Category.hasMany(Category)
+
+sequelize.sync()
 
 var accountsRouter = require('./routes/accounts')
 var budgetsRouter = require('./routes/budgets')
